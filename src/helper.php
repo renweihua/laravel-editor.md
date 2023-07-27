7f59f00e84924772d699d346d5426fa083a2a6f4
小丑路人 <2278757482@qq.com>
2023-07-27 15:39:54 +0800

    add editor_only_js

--- src/helper.php
+++ src/helper.php
@@ -12,18 +12,26 @@ if (!function_exists("editor_css")) {
     }
 }
 
+
+if (!function_exists("editor_only_js")) {
+    function editor_only_js()
+    {
+        return '
+            <script src="/vendor/editormd/js/editormd.js"></script>
+            <script src="/vendor/editormd/lib/marked.min.js"></script>
+            <script src="/vendor/editormd/lib/prettify.min.js"></script>
+            <script src="/vendor/editormd/lib/raphael.min.js"></script>
+            <script src="/vendor/editormd/lib/underscore.min.js"></script>
+            <script src="/vendor/editormd/lib/sequence-diagram.min.js"></script>
+            <script src="/vendor/editormd/lib/flowchart.min.js"></script>
+            <script src="/vendor/editormd/lib/jquery.flowchart.min.js"></script>';
+    }
+}
+
 if (!function_exists("editor_js")) {
     function editor_js()
     {
-        return '
-<script src="/vendor/editormd/js/editormd.js"></script>
-<script src="/vendor/editormd/lib/marked.min.js"></script>
-<script src="/vendor/editormd/lib/prettify.min.js"></script>
-<script src="/vendor/editormd/lib/raphael.min.js"></script>
-<script src="/vendor/editormd/lib/underscore.min.js"></script>
-<script src="/vendor/editormd/lib/sequence-diagram.min.js"></script>
-<script src="/vendor/editormd/lib/flowchart.min.js"></script>
-<script src="/vendor/editormd/lib/jquery.flowchart.min.js"></script>
+        return editor_only_js() . '
 <script>
     var testEditor;
     $(function () {
